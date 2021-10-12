853f471a0440c4520aa359e3c330bab83e0a0fca
Michael Blank <michi2806@gmail.com>
2021-10-12 18:28:30 +0200

    Added a/d controls (#4)
    Co-authored-by: Michael Blank <e11909459@student.tuwien.ac.at>

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -22,7 +22,7 @@ func _ready():
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
 	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
 	
 	if input_vector != Vector2.ZERO:
 		m_animation_tree.set("parameters/idle/blend_position", input_vector.x);
